8dd1844eaa2af63550a6a3b9d7614ca1a1032466
Feifx <32243939+faysoserious@users.noreply.github.com>
2021-06-21 14:26:20 +0200

    first 10 steps

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+      return <h1>Hello world</h1>;
+    }
+  }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        < img src="" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
