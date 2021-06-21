edc05a003b4ce9df0b0bc25450606bf2f44de8c8
Feifx <32243939+faysoserious@users.noreply.github.com>
2021-06-21 15:07:01 +0200

    step 27

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,27 @@
 //import logo from './logo.svg';
-import './App.css';
 import React from 'react';
+import { render } from 'react-dom';
+import './App.css';
+
 
 class App extends React.Component {
-    render() {
-      return <h1>Hello world</h1>;
-    }
-  }
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        < img src="" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <!-- Add a SearchBar component -->
+          <div className="App-playlist">
+            <!-- Add a SearchResults component -->
+            <!-- Add a Playlist component -->
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
 }
+
+
 
 export default App;
---
+++ src/components/TrackLists/TrackLists.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TrackLists.css';
+export class TrackLists extends React.Component {
+    render() {
+        return ( <div className="TrackList">
+        <!-- You will add a map method that renders a set of Track components  -->
+    </div>
+        );
+    }
+}
