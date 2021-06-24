48e3ab779dff748138c0cef3510d85b4f81fe28f
Feifx <32243939+faysoserious@users.noreply.github.com>
2021-06-24 15:38:49 +0200

    step 32

--- src/components/Playlists/Playlists.js
+++ src/components/Playlists/Playlists.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './Playlists.css';
 import { TrackLists } from '../TrackLists/TrackLists';
+
 export class Playlists extends React.Component {
     render() {
-        return ( <
-            div className = "Playlist" >
-            <
-            input defaultValue = { 'New Playlist' }
-            />
-            <!-- Add a TrackList component -->
-            <
-            button className = "Playlist-save" > SAVE TO SPOTIFY < /button> < /
-            div >
-        );
+        return (<div className="Playlist" >
+            <input defaultValue={'New Playlist'}/>
+            {/* <!-- Add a TrackList component --> */}
+            <TrackLists />
+            <button className="Playlist-save" > SAVE TO SPOTIFY </button> 
+            </div >
+                );
 
     }
 }
--- src/components/TrackLists/TrackLists.js
+++ src/components/TrackLists/TrackLists.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './TrackLists.css';
 import { Track } from '../Track/Track';
+
 export class TrackLists extends React.Component {
     render() {
         return ( <div className="TrackList">
-        <!-- You will add a map method that renders a set of Track components  -->
+        {/* <!-- You will add a map method that renders a set of Track components  --> */}
     </div>
         );
     }
 }
+
